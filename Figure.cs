4400f6ae2ad8964d1b16e317033798cc6d29e323
kiragit01 <kiragithub@gmail.com>
2024-05-23 22:49:16 +0300

    Добавьте файлы проекта.

---
+++ Figure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP3
+{
+    class Figure
+    {
+        Point A, B, C, D, E;
+        private string name;
+        public Figure(Point A, Point B, Point C) : this(A, B, C, C) { name = "Треугольник"; }
+        public Figure(Point A, Point B, Point C, Point D) : this(A, B, C, D, D) { name = "Четырехугольник"; }
+        public Figure(Point A, Point B, Point C, Point D, Point E)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.D = D;
+            this.E = E;
+            name = "Пятиугольник";
+        }
+
+        double LengthSide(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow(A.X - B.X, 2) + Math.Pow(A.Y - B.Y, 2));
+        }
+
+        public void PerimeterCalculator()
+        {
+            Console.WriteLine($"Фигура - {name}");
+            Console.WriteLine($"Площадь: {(LengthSide(A, B) + LengthSide(B, C) + LengthSide(C, D) + LengthSide(D, E) + LengthSide(E, A)):f2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point a = new Point(1, 1, "a");
+            Point b = new Point(3, 2, "b");
+            Point c = new Point(3, 4, "c");
+            Point d = new Point(6, 2, "d");
+            Point e = new Point(5, 5, "e");
+
+            Figure figure1 = new Figure(a, b, c);
+            Figure figure2 = new Figure(a, b, c, d);
+            Figure figure3 = new Figure(a, b, c, d, e);
+
+            figure1.PerimeterCalculator();
+            figure2.PerimeterCalculator();
+            figure3.PerimeterCalculator();
+
+            Console.ReadKey();
+        }
+
+    }
+}
